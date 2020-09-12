917be56470264e2fb57163b9e5decef8f94a1d26
Micheal <michealnkim-wm19@student.tarc.edu.my>
2020-09-12 13:31:09 +0800

    Initial commit

---
+++ app/src/main/java/PolicyTitle.java
@@ -0,0 +1,12 @@
+public class PolicyTitle {
+    String title;
+    public PolicyTitle(){}
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
